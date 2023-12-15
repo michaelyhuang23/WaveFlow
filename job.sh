16a44f2825e7d469ddb701c28f31e561dbbfc39c
Michael Huang <michaelyhuang23@gmail.com>
2023-12-15 14:32:36 -0500

    job

---
+++ job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:volta:1
+#SBATCH --cpus-per-task=20
+#SBATCH -o train_waveflow.txt
+#SBATCH --job-name=train_waveflow
+
+module load anaconda/2023a
+
+python train.py -c configs/waveflow-h16-r64-bipartize.json
